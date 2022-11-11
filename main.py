e7564cd39dd02313e5e53b0c8ee0a8e75ef75db5
Eryk <erwinek1998@gmail.com>
2022-11-11 22:21:04 +0100

    dialog quick fix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import sys
 
+import matplotlib.pyplot as plt
+import numpy as np
 from PyQt5.QtWidgets import QApplication
 
 from UI.main_window import MainWindowDialog
@@ -12,3 +14,8 @@ main_dlg = MainWindowDialog()
 main_dlg.show_dialog()
 
 main_app.exec()
+
+# xpoints = np.array([150, 187])
+#
+# plt.plot(xpoints)
+# plt.show()
---
+++ utils.py
@@ -0,0 +1,6 @@
+from PyQt5.QtWidgets import QFileDialog
+
+
+class CustomFileWidget(QFileDialog):
+    def __init__(self):
+        pass
